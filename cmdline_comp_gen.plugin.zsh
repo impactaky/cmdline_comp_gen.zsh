9135e5db850be90b6c3f814193aa6a4f033f42a3
impactaky <impactaky@users.noreply.github.com>
2020-08-23 18:16:04 +0900

    Implment cmdline_comp_gen.plugin.zsh

---
+++ cmdline_comp_gen.plugin.zsh
@@ -0,0 +1,53 @@
+[ $CMDLINE_COMP_GEN_ZSH_CONFIG_DIR ] || export CMDLINE_COMP_GEN_ZSH_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/cmdline_comp_gen.zsh"
+if [[ ! -e $CMDLINE_COMP_GEN_ZSH_CONFIG_DIR ]]; then
+    mkdir -p $CMDLINE_COMP_GEN_ZSH_CONFIG_DIR
+fi
+[ $CMDLINE_COMP_GEN_ZSH_CACHE_DIR ] || export CMDLINE_COMP_GEN_ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/cmdline_comp_gen.zsh"
+if [[ ! -e $CMDLINE_COMP_GEN_ZSH_CACHE_DIR ]]; then
+    mkdir -p $CMDLINE_COMP_GEN_ZSH_CACHE_DIR
+fi
+
+function __cmdline_comp_gen::getCompsyncArg () {
+    option=$1
+    desc=${@:2}
+    desc=${desc:gs/"'"/}
+    desc=${desc:gs/[/\\[}
+    desc=${desc:gs/]/\\]}
+    if [[ $desc ]]; then
+        echo "'"$option'['$desc']'"' \\"
+    else
+        echo "'"$option"' \\"
+    fi
+}
+
+function cmdline_comp_gen () {
+    local command_name=$(basename $1)
+    local output_file=$CMDLINE_COMP_GEN_ZSH_CACHE_DIR/$command_name.zsh
+    cat <<EOF > $output_file
+compdef __cmdline_comp_gen::comp::$command_name $command_name
+function __cmdline_comp_gen::comp::$command_name () {
+if [[ -e \$CMDLINE_COMP_GEN_ZSH_CONFIG_DIR/$command_name ]]; then
+    overwride_args=(\$(cat \$CMDLINE_COMP_GEN_ZSH_CONFIG_DIR/$command_name))
+fi
+_arguments : \$overwride_args \\
+'*:argument:_files' \\
+EOF
+    $1 --help |& while read -r message; do
+        if [[ "$message" =~ "^ *options:$" ]] || [[ "$message" =~ "^ *usage:.*$" ]]; then
+            continue
+        fi
+        arr=($=message)
+        if [[ $arr[1] =~ ",$" ]]; then
+            __cmdline_comp_gen::getCompsyncArg ${arr[1]%,} ${arr:3} >> $output_file
+            __cmdline_comp_gen::getCompsyncArg $arr[2] ${arr:3} >> $output_file
+        else
+            __cmdline_comp_gen::getCompsyncArg $arr[1] ${arr:2} >> $output_file
+        fi
+    done
+    cat <<EOF >> $output_file
+
+}
+EOF
+}
+
+source $CMDLINE_COMP_GEN_ZSH_CACHE_DIR/*
